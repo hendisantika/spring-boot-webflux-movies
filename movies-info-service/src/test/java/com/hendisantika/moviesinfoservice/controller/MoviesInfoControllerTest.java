bc3cd7ec3b6f05100e8a1acd058684ec8c7000ce
hendisantika <hendisantika@yahoo.co.id>
2024-01-03 08:21:07 +0700

    Add getMovieInfoById in MoviesInfoControllerTest in movies-info-service 🫸🌀✏️📗 :octocat:

--- movies-info-service/src/test/java/com/hendisantika/moviesinfoservice/controller/MoviesInfoControllerTest.java
+++ movies-info-service/src/test/java/com/hendisantika/moviesinfoservice/controller/MoviesInfoControllerTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 
 /**
@@ -51,5 +54,25 @@ class MoviesInfoControllerTest {
                 .isOk()
                 .expectBodyList(MovieInfo.class)
                 .hasSize(3);
+    }
+
+    @Test
+    void getMovieInfoById() {
+        var movieInfo = new MovieInfo("specific-id", "Dark Knight Rises", 2012, List.of("Christian Bale"), LocalDate.parse("2012-07-20"));
+        when(moviesInfoServiceMock.getMovieInfoById("specific-id"))
+                .thenReturn(Mono.just(movieInfo));
+
+        client
+                .get()
+                .uri("/v1/movieinfos/{id}", "specific-id")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    var response = movieInfoEntityExchangeResult.getResponseBody();
+                    assertThat(response.getId(), equalTo("specific-id"));
+                    assertThat(response.getName(), equalTo("Dark Knight Rises"));
+                });
     }
 }
