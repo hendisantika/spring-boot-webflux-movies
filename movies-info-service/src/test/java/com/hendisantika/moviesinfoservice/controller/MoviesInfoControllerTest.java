3a2663fc41d5e208fd6bdc5193359ca6fa2164e9
hendisantika <hendisantika@yahoo.co.id>
2024-01-03 08:20:34 +0700

    Add getAllMoviesInfo in MoviesInfoControllerTest in movies-info-service 🫸🌀✏️📗 :octocat:

--- movies-info-service/src/test/java/com/hendisantika/moviesinfoservice/controller/MoviesInfoControllerTest.java
+++ movies-info-service/src/test/java/com/hendisantika/moviesinfoservice/controller/MoviesInfoControllerTest.java
@@ -1,11 +1,19 @@
 package com.hendisantika.moviesinfoservice.controller;
 
+import com.hendisantika.moviesinfoservice.entity.MovieInfo;
 import com.hendisantika.moviesinfoservice.service.MoviesInfoService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +33,23 @@ class MoviesInfoControllerTest {
 
     @MockBean
     private MoviesInfoService moviesInfoServiceMock;
+
+    @Test
+    void getAllMoviesInfo() {
+        var movieInfos = List.of(
+                new MovieInfo(null, "Batman Begins", 2005, List.of("Christian Bale"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight", 2008, List.of("Christian Bale"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("specific-id", "Dark Knight Rises", 2012, List.of("Christian Bale"), LocalDate.parse("2012-07-20"))
+        );
+        when(moviesInfoServiceMock.getAllMovieInfos())
+                .thenReturn(Flux.fromIterable(movieInfos));
+
+        client.get()
+                .uri("/v1/movieinfos")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(MovieInfo.class)
+                .hasSize(3);
+    }
 }
