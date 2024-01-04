ffd4b8d107c0cdb998e2cbddce47d56be56967e4
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 07:55:37 +0700

    Add setup in MoviesReviewServiceApplicationTests in movies-review-service 🫸🌀✏️📗🐧🐳⬆

--- movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/MoviesReviewServiceApplicationTests.java
+++ movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/MoviesReviewServiceApplicationTests.java
@@ -1,11 +1,14 @@
 package com.hendisantika.moviesreviewservice;
 
+import com.hendisantika.moviesreviewservice.domain.Review;
 import com.hendisantika.moviesreviewservice.repository.ReviewReactiveRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -17,8 +20,13 @@ class MoviesReviewServiceApplicationTests {
     @Autowired
     private ReviewReactiveRepository repository;
 
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    void setup() {
+        var reviews = List.of(
+                new Review(null, 1L, "Awesome movie", 9.0),
+                new Review(null, 1L, "Great movie", 8.0),
+                new Review("specific-id", 2L, "Best movie", 9.0)
+        );
+        repository.saveAll(reviews).blockLast();
     }
-
 }
