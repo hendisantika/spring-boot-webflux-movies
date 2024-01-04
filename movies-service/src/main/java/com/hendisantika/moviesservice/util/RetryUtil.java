0c13a71896d3abc50bfdeb0652e95f65289ffed7
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 08:08:16 +0700

    Add RetryUtil in movies-service 🫸🌀✏️📗🐧🐳

---
+++ movies-service/src/main/java/com/hendisantika/moviesservice/util/RetryUtil.java
@@ -0,0 +1,28 @@
+package com.hendisantika.moviesservice.util;
+
+import reactor.core.Exceptions;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-movies
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/4/24
+ * Time: 08:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class RetryUtil {
+    public static Retry retrySpec(Class<?> clazz) {
+        var retrySpec = Retry.fixedDelay(3, Duration.ofMillis(500))
+                .filter(ex -> clazz.isAssignableFrom(ex.getClass()))
+                .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
+                    return Exceptions.propagate(retrySignal.failure());
+                });
+
+        return retrySpec;
+    }
+}
