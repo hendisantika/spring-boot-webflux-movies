0d7720c8d0e2eb731a553cd03122c5f230490919
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 08:11:03 +0700

    Add retrieveReviews in ReviewRestClient in movies-service 🫸🌀✏️📗🐧🐳

--- movies-service/src/main/java/com/hendisantika/moviesservice/client/ReviewRestClient.java
+++ movies-service/src/main/java/com/hendisantika/moviesservice/client/ReviewRestClient.java
@@ -1,9 +1,17 @@
 package com.hendisantika.moviesservice.client;
 
+import com.hendisantika.moviesservice.domain.Review;
+import com.hendisantika.moviesservice.exception.ReviewsClientException;
+import com.hendisantika.moviesservice.exception.ReviewsServerException;
+import com.hendisantika.moviesservice.util.RetryUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +31,30 @@ public class ReviewRestClient {
 
     @Value("${restClient.reviewsUrl}")
     private String reviewUrl;
+
+    public Flux<Review> retrieveReviews(String movieId) {
+        var uri = UriComponentsBuilder.fromHttpUrl(reviewUrl)
+                .queryParam("movieInfoId", movieId)
+                .buildAndExpand()
+                .toUriString();
+
+        return client
+                .get()
+                .uri(uri)
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.empty();
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(message -> Mono.error(new ReviewsClientException(message)));
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(message -> Mono.error(new ReviewsServerException(message)));
+                })
+                .bodyToFlux(Review.class)
+                .retryWhen(RetryUtil.retrySpec(ReviewsServerException.class))
+                .log();
+    }
 }
