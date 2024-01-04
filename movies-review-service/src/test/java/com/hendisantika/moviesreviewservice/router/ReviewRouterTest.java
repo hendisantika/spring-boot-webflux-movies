1b911ad8838fba3081158c834d56986e51d9f94d
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 07:52:17 +0700

    Add addReview in ReviewRouterTest in movies-review-service 🫸🌀✏️📗🐧🐳⬆

--- movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/router/ReviewRouterTest.java
+++ movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/router/ReviewRouterTest.java
@@ -1,14 +1,23 @@
 package com.hendisantika.moviesreviewservice.router;
 
+import com.hendisantika.moviesreviewservice.domain.Review;
 import com.hendisantika.moviesreviewservice.exception.GlobalExceptionHandler;
 import com.hendisantika.moviesreviewservice.handler.ReviewHandler;
 import com.hendisantika.moviesreviewservice.repository.ReviewReactiveRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +39,25 @@ class ReviewRouterTest {
 
     @Autowired
     private WebTestClient client;
+
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Good movie", 7.5);
+
+        when(repository.save(review))
+                .thenReturn(Mono.just(new Review("id", 1L, "Good movie", 7.5)));
+        client
+                .post()
+                .uri("/v1/reviews")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewEntityExchangeResult -> {
+                    var response = reviewEntityExchangeResult.getResponseBody();
+                    assertThat(response, is(not(nullValue())));
+                });
+    }
 
 }
