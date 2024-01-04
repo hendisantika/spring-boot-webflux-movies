893617f501cbf09714cd896c5e7f10a0d114b153
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 08:12:43 +0700

    Add retrieveMovieById in MoviesController in movies-service 🫸🌀✏️📗🐧🐳

--- movies-service/src/main/java/com/hendisantika/moviesservice/controller/MoviesController.java
+++ movies-service/src/main/java/com/hendisantika/moviesservice/controller/MoviesController.java
@@ -2,9 +2,13 @@ package com.hendisantika.moviesservice.controller;
 
 import com.hendisantika.moviesservice.client.MoviesInfoRestClient;
 import com.hendisantika.moviesservice.client.ReviewRestClient;
+import com.hendisantika.moviesservice.domain.Movie;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +27,14 @@ public class MoviesController {
 
     private final MoviesInfoRestClient moviesInfoRestClient;
     private final ReviewRestClient reviewRestClient;
+
+    @GetMapping("/{movieId}")
+    public Mono<Movie> retrieveMovieById(@PathVariable String movieId) {
+        return moviesInfoRestClient.retrieveMovieInfo(movieId)
+                .flatMap(movieInfo -> {
+                    var reviewsMono = reviewRestClient.retrieveReviews(movieInfo.getMovieInfoId())
+                            .collectList();
+                    return reviewsMono.map(reviews -> new Movie(movieInfo, reviews));
+                });
+    }
 }
