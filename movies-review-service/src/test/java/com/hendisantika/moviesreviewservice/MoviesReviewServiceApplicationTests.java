c4d26630621106461a2ae5828f4a2b6690d56817
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 07:55:19 +0700

    Add MoviesReviewServiceApplicationTests in movies-review-service 🫸🌀✏️📗🐧🐳⬆

--- movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/MoviesReviewServiceApplicationTests.java
+++ movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/MoviesReviewServiceApplicationTests.java
@@ -1,10 +1,21 @@
 package com.hendisantika.moviesreviewservice;
 
+import com.hendisantika.moviesreviewservice.repository.ReviewReactiveRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class MoviesReviewServiceApplicationTests {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Autowired
+    private ReviewReactiveRepository repository;
 
     @Test
     void contextLoads() {
