107ee48369acce85b17979fe460cf2bc8a695da5
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 07:56:44 +0700

    Add addReview in MoviesReviewServiceApplicationTests in movies-review-service 🫸🌀✏️📗🐧🐳⬆

--- movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/MoviesReviewServiceApplicationTests.java
+++ movies-review-service/src/test/java/com/hendisantika/moviesreviewservice/MoviesReviewServiceApplicationTests.java
@@ -4,12 +4,18 @@ import com.hendisantika.moviesreviewservice.domain.Review;
 import com.hendisantika.moviesreviewservice.repository.ReviewReactiveRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -34,5 +40,22 @@ class MoviesReviewServiceApplicationTests {
     @AfterEach
     void tearDown() {
         repository.deleteAll().block();
+    }
+
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Good movie", 7.5);
+        client
+                .post()
+                .uri("/v1/reviews")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewEntityExchangeResult -> {
+                    var response = reviewEntityExchangeResult.getResponseBody();
+                    assertThat(response, is(not(nullValue())));
+                });
     }
 }
