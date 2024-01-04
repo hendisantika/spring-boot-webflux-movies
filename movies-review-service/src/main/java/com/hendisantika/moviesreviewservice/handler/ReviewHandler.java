6bc6f864ee32c5cb877e79a051e67dcf9814efb3
hendisantika <hendisantika@yahoo.co.id>
2024-01-04 07:45:17 +0700

    Add addReview in ReviewHandler in movies-review-service 🫸🌀✏️📗🐧🐳⬆

--- movies-review-service/src/main/java/com/hendisantika/moviesreviewservice/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/hendisantika/moviesreviewservice/handler/ReviewHandler.java
@@ -2,7 +2,11 @@ package com.hendisantika.moviesreviewservice.handler;
 
 import com.hendisantika.moviesreviewservice.domain.Review;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 /**
@@ -22,4 +26,15 @@ public class ReviewHandler {
     private final ReviewReactiveRepository repository;
     private final Validator validator;
     private final Sinks.Many<Review> reviewSink = Sinks.many().replay().latest();
+
+    public Mono<ServerResponse> addReview(ServerRequest request) {
+        return request.bodyToMono(Review.class)
+                .doOnNext(this::validate)
+                .flatMap(repository::save)
+                .doOnNext(review -> {
+                    reviewSink.tryEmitNext(review);
+                })
+                .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
+    }
+
 }
